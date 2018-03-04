937ef5b475bcdeb912d2263de671f62311eaafc1
Viktor Ceder <vceder@kth.se>
2018-03-04 21:09:04 +0100

    interim

--- client/src/components/StarCounter.vue
+++ client/src/components/StarCounter.vue
@@ -1,13 +1,27 @@
 <template lang="html">
     <div class="starCounter">
-        Stars: xxxx 
+        Stars: {{starCount}} <i class="fas fa-user"></i>
+
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
+    computed: {
+        ...mapState(['starCount']),
+    },
+    data() {
+        return {
+            watchers: this.starCount
+        }
+    }
 }
 </script>
 
 <style lang="css">
+    .starCounter{
+        color: #9FFF70;
+    }
 </style>
